29a7f40a2531f942cc0374e5ba0b9b2c3af867a9
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-16 17:50:15 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -45,7 +45,7 @@ def hit?(card_total)
   input = get_user_input
   if input == "s"
     display_card_total(card_total)
-    prompt_user
+
   elsif input == "h"
     card_total += deal_card
     display_card_total(card_total)
